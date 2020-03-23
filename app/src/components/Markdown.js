8a665e279edce63c2c5bab43afa0a04cc90b0ea1
Sem <me@sems.dev>
2020-03-23 14:58:21 +0100

    Styling title of markdown

--- app/src/components/Markdown.js
+++ app/src/components/Markdown.js
@@ -15,7 +15,7 @@ export class Markdown extends React.Component {
 	render() {
 		return(
 			<div className="__markdown">
-				<h1>{this.props.title}</h1>
+				<h1 className="__markdown_title">{this.props.title}</h1>
                 <div dangerouslySetInnerHTML={{__html: this.props.content}}></div>
 				<hr></hr>
 			</div>
