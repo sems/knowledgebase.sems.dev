006ecd4802a68909e43c72f7e2e9741b8d2da3d1
Sem <me@sems.dev>
2020-03-23 11:51:26 +0100

    Fixed menubar error

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
-import { ClientContent } from './components/ClientContent';
-import { ServerContent } from './components/ServerContent';
-import { SecurityContent } from './components/SecurityContent';
+import {Home} from './components/Home';
+import {ClientContent} from './components/ClientContent';
+import {ServerContent} from './components/ServerContent';
+import {SecurityContent} from './components/SecurityContent';
 
 import './styles/main.sass';
 
@@ -15,36 +16,39 @@ class App extends Component {
       <div className="home_container">
         <Router>
           <div>
-          <nav className="mainMenu">
-            <ul className="mainMenu_list">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/client">Client</Link>
-              </li>
-              <li>
-                <Link to="/server">Server</Link>
-              </li>
-              <li>
-                <Link to="/security">Security</Link>
-              </li>
-            </ul>
+            <nav className="mainMenu">
+              <ul className="mainMenu_list">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/client">Client</Link>
+                </li>
+                <li>
+                  <Link to="/server">Server</Link>
+                </li>
+                <li>
+                  <Link to="/security">Security</Link>
+                </li>
+              </ul>
 
-            <hr></hr>
+              <hr></hr>
 
-            <Switch>
-            <Route exact path="/client">
-              <ClientContent />
-            </Route>
-            <Route exact path="/server">
-              <SecurityContent />
-            </Route>
-            <Route exact path="/security">
-              <ServerContent />
-            </Route>
-          </Switch>
-          </nav>
+              <Switch>
+                <Route exact path="/">
+                  <Home/>
+                </Route>
+                <Route exact path="/client">
+                  <ClientContent/>
+                </Route>
+                <Route exact path="/server">
+                  <ServerContent/>
+                </Route>
+                <Route exact path="/security">
+                  <SecurityContent/>
+                </Route>
+              </Switch>
+            </nav>
           </div>
         </Router>
       </div>
