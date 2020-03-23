32f318c7f0a1178f802364c76e8e7b27e9664f1f
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 01:50:49 +0100

    content providers

---
+++ app/src/components/ServerContent.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Prism from "prismjs";
+import marked from "marked";
+
+import { Post } from './post';
+
+import '../styles/content.sass';
+import '../styles/prism.css';
+import '../styles/page.sass';
+
+const importAll = (r) => r.keys().map(r);
+const markdownFiles = importAll(require.context('../assets/tasker/server', false, /\.md$/))
+	.sort(function(a, b) {
+		var aa = a.substring(19).split('.');
+		var bb = b.substring(19).split('.');
+		if (parseInt(aa[0]) === parseInt(bb[0])) {
+			return parseInt(aa[1].split('_')[0]) < parseInt(bb[1].split('_')[0]) ? -1 : 1;
+		}
+		return parseInt(aa[0]) < parseInt(bb[0]) ? -1 : 1;
+	});
+
+export class ServerContent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			posts: [],
+		};
+	}
+
+	async componentDidMount() {
+		const posts = await Promise.all(
+			markdownFiles.map((file) => fetch(file).then((res) => res.text()))
+		).catch((err) => console.error(err));
+  
+		  this.setState((state) => ({ ...state, posts }));
+		  this.setState({
+			postTitles: markdownFiles
+		  });  
+	}
+	
+	componentWillMount() {
+		marked.setOptions({
+			breaks: true,
+			highlight: function (code, lang) {
+				try {
+					return Prism.highlight(code, Prism.languages[lang], lang);
+				} catch {
+					return code;
+				}
+			}
+		});
+	}
+
+	render() {
+		const { posts } = this.state;
+		
+		return ( 
+			<div className="front-header">
+				<div className="main">
+					<div className="client_content">
+						{
+							posts.map((post, id) => (
+								<Post key={id} content={marked(post)}></Post>
+							))
+						}
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
