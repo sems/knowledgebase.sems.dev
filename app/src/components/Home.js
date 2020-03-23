f3d886a7b595c89fe23e8fec980b13c14043ea1f
Sem <me@sems.dev>
2020-03-23 11:51:45 +0100

    Added home-page

---
+++ app/src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {	
+
+		};
+	}
+
+	render() {
+		return(
+            <div className="front-header">
+				<div className="main">
+                    <div className="page_home">
+                        <h1>Welkom om de tasker</h1>
+                        <h2>Maar dan niet op tasker</h2>
+                    </div>
+                </div>
+            </div>
+            
+		)
+	}
+}
+  
